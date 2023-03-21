f18ca132c2ec5bea66bdfb936a9ee33f26715a0e
Nathan Huber <nathan_huber@yahoo.com>
2023-03-21 00:15:59 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,6 @@ export default {
       let d = new Date();
       let months = ["January", "February", " March", " April ", "May", "June", "July", "August", "September", "October", "November", "December"];
       let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
       let day = days[d.getDay()];
       let date = d.getDate();
       let month = months[d.getMonth()];
